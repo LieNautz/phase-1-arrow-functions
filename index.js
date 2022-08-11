8141eeb67ff37d3a37d5dfa99c0b54059997b698
LieNautz <tuan_lai01@hotmail.com>
2022-08-11 21:40:02 +1000

    completed

---
+++ index.js
@@ -0,0 +1,14 @@
+//Create a function expression called divide 
+
+let divide= function (){
+return (2000/100);
+}
+
+let square= (num1) => {
+    return num1 * num1;
+}
+
+let add= (add1, add2) => {
+    return add1+add2;
+
+}
